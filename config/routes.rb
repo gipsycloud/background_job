a3d2704c5d212ec14649d48a41f3c8a873a7f45a
aungthi <aungthiwin1801@gmail.com>
2023-04-24 13:03:20 +0630

    adding devise gem for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  mount Sidekiq::Web => 'sidekiq'
+  authenticate :user do
+    mount Sidekiq::Web => 'sidekiq'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
