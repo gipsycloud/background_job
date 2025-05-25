4aaeaf17a415ab5e4438055e525f31294d3ea14c
webgipsy <babybird1801@gmail.com>
2025-05-25 19:14:01 +0630

    adding custom css

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,4 @@
+# filepath: /home/aung/Desktop/rails_project/video/config/importmap.rb
+pin "application", to: "application.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
